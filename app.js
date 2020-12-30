31d270746a3261ad6ddca7e74a9817426b0430f8
lcarpignano <lcarpignano@gmail.com>
2020-12-30 20:35:53 -0300

    frontend listo

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/index.html'))
+})
+
+app.get('/register.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'))
+})
+
+app.get('/login.html', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'))
+})
+
+app.listen(3000, () => console.log('Ya podes ir a http://localhost:3000'))
+
