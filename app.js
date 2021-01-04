3c7465a43e05b922e99b4a4a9c453609ace83464
lcarpignano <lcarpignano@gmail.com>
2021-01-04 20:25:54 -0300

    Heroku

--- app.js
+++ app.js
@@ -17,5 +17,5 @@ app.get('/login.html', (req, res) => {
     res.sendFile(path.join(__dirname, '/views/login.html'))
 })
 
-app.listen(3000, () => console.log('Ya podes ir a http://localhost:3000'))
+app.listen(process.env.PORT || 3000, () => console.log('Ya podes ir a http://localhost:3000'))
 
